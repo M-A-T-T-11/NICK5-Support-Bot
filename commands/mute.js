0ce21ca9c9211e1362ee9de2bfadf167567119eb
N1ckStern <itzN3k0corp@gmail.com>
2021-01-14 11:39:01 -0600

    Initial Commit

--- commands/mute.js
+++ commands/mute.js
@@ -5,9 +5,13 @@ module.exports = {
           const target = message.mentions.users.first();
           if(target){
               let mainRole = message.guild.roles.cache.find(role => role.name === 'Pleb');   
-              let muteRole = message.guild.roles.cache.find(role => role.name === 'muted');
+              let muteRole = message.guild.roles.cache.find(role => role.name === 'mute');
               
-              
+              let memberTarget= message.guild.members.cahce.get(target.id);
+
+              memberTarget.roles.remove(mainRole.id);
+              memberTarget.roles.add(muteRole.id);
+              message.channel.send(`<@${memberTarget.user.id}> has been muted`);
           } else{
               message.channel.send('Cant find that member');
           }
