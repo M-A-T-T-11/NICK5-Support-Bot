4fa2d69b05c8709bb936a811cc4c8340d5c9cf53
N1ckStern <itzN3k0corp@gmail.com>
2021-01-14 11:50:51 -0600

    no

--- commands/AcPr.js
+++ commands/AcPr.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'acpr',
     description: "this is an admin command!",
     execute(message, args){
-           message.channel.send('Tu peticion ha sido enviada a los miembros de STAFF pronto uno de ellos te atendera y veremos si eres apto para ser programador del server :)');
+           message.channel.send('Los admins son los managers de este server estan a cargo de mantener el orden y el respeto para que podamos convivir en armonia, si los admins no estan los Vigilantes se quedan a cargo de esta tarea ambos roles tienen permiso de mutear, kickear o banear a cualquier usuario si asi lo consideran. Este rol es otorgado por Niko a aquellos que el considera disgnos :)');
     }
 }
